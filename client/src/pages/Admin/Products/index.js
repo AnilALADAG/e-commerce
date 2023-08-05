e661ce3711680e7d6e0e2d4012aa419f33157a3d
AnilALADAG <anilaladag@gmail.com>
2023-08-05 19:39:56 +0300

    admin order and product implementations

--- client/src/pages/Admin/Products/index.js
+++ client/src/pages/Admin/Products/index.js
@@ -1,7 +1,88 @@
-import React from 'react';
+import { useMemo } from 'react';
+
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { fetchProductList, deleteProduct } from '../../../api';
+import { Table, Popconfirm } from 'antd';
+import { Button, Flex } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 function Products() {
-  return <div>Products</div>;
+  const queryClient = useQueryClient();
+
+  const { isLoading, isError, data, error } = useQuery(
+    'admin:products',
+    fetchProductList
+  );
+
+  const deleteMutation = useMutation(deleteProduct, {
+    onSuccess: () => queryClient.invalidateQueries('admin:products'),
+  });
+
+  const columns = useMemo(() => {
+    return [
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        key: 'title',
+      },
+      {
+        title: 'Price',
+        dataIndex: 'price',
+        key: 'price',
+      },
+      {
+        title: 'Created At',
+        dataIndex: 'createdAt',
+        key: 'createdAd',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <>
+            <Link to={`admin/products/${record._id}`}>Edit</Link>
+            <Popconfirm
+              title="Are you sure?"
+              onConfirm={() => {
+                deleteMutation.mutate(record._id, {
+                  onSuccess: () => {
+                    console.log('success');
+                  },
+                });
+              }}
+              onCancel={() => console.log('cancelled')}
+              okText="Yes"
+              cancelText="No"
+              placement="left"
+            >
+              <a style={{ marginLeft: '10px' }}>Delete</a>
+            </Popconfirm>
+          </>
+        ),
+      },
+    ];
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error {error.message}</div>;
+  }
+
+  console.log(data);
+  return (
+    <div>
+      <Flex justifyContent="right" pb="2">
+        <Link to="/admin/products/new">
+          <Button colorScheme="blue">New</Button>
+        </Link>
+      </Flex>
+
+      <Table dataSource={data} columns={columns} rowKey="_id" bordered={true} />
+    </div>
+  );
 }
 
 export default Products;
--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -7,6 +7,8 @@ import { Link, Switch, Route, useRouteMatch } from 'react-router-dom';
 import Home from './Home';
 import Orders from './Orders';
 import Products from './Products';
+import ProductDetail from './ProductDetail';
+import NewProduct from './Products/new';
 
 function Admin() {
   const { path, url } = useRouteMatch();
@@ -34,6 +36,11 @@ function Admin() {
           <Route exact path={path} component={Home} />
           <Route path={`${path}/orders`} component={Orders} />
           <Route exact path={`${path}/products`} component={Products} />
+          <Route exact path={`${path}/products/new`} component={NewProduct} />
+          <Route
+            path={`${path}/admin/products/:product_id`}
+            component={ProductDetail}
+          />
         </Switch>
       </Box>
     </div>
