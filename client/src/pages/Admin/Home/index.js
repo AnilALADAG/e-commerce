385a9a66edb681f80fa5d704a44cd549aad97bff
AnilALADAG <anilaladag@gmail.com>
2023-08-05 15:00:14 +0300

    admin pages added

---
+++ client/src/pages/Admin/Home/index.js
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <Box textAlign="center" fontSize="2xl">
+        Admin Homee
+      </Box>
+    </div>
+  );
+}
+
+export default Home;
